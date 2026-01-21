90ca2f221af194d52577a0ed25bf485fc478fc4b
Mert <114104396+wezmert@users.noreply.github.com>
2026-01-21 13:40:24 +0300

    Add basic Telegram bot functionality

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+import requests
+import time
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+def telegram_mesaj(mesaj):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": mesaj
+    }
+    requests.post(url, data=data)
+
+if name == "main":
+    telegram_mesaj("✅ Bot çalıştı. Bu test mesajıdır.")
+    while True:
+        time.sleep(300)
